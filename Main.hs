04e4ff287385a6e2d6bd1fffb7af045d73803d36
Max Gonzih <gonzih@gmail.com>
2014-08-09 22:13:53 +0200

    First steps

--- Main.hs
+++ Main.hs
@@ -1,17 +1,36 @@
-{-# LANGUAGE OverloadedStrings #-} -- for FilePath literals
+{-# LANGUAGE OverloadedStrings #-}
 
 import System.FSNotify
+import System.Process (system)
+import System.Exit (ExitCode(..))
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
+import Filesystem.Path.CurrentOS (encodeString)
+import Control.Concurrent.MVar
 
-main =
+eventType :: Event -> String
+eventType (Added _ _)    = "added"
+eventType (Modified _ _) = "modified"
+eventType (Removed _ _)  = "removed"
+
+handler :: Event -> IO ()
+handler event = do
+    let path = encodeString $ eventPath event
+        eType = eventType event
+        cmd = "./.fswatch \"" ++ path  ++ "\" \"" ++ eType ++ "\""
+    code <- system cmd
+    case code of
+      ExitFailure _ -> putStrLn "Execution of helper failed." >> print code
+      _             -> return ()
+
+main :: IO ()
+main = do
+  putStrLn "Watching current directory for changes."
   withManager $ \mgr -> do
-    -- start a watching job (in the background)
-    watchDir
+    _ <- watchTree
       mgr          -- manager
       "."          -- directory to watch
       (const True) -- predicate
-      print        -- action
+      handler      -- action
 
-    -- sleep forever (until interrupted)
     forever $ threadDelay maxBound
