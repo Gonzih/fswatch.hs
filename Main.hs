3b29bf9580e547ec63adb267622166f2a0a8f6db
Max Gonzih <gonzih@gmail.com>
2014-08-10 12:39:07 +0200

    Ignore .fswatch file (causing no file error due to writes)

--- Main.hs
+++ Main.hs
@@ -5,21 +5,38 @@ import System.Process (system)
 import System.Exit (ExitCode(..))
 import Control.Monad (forever)
 import Filesystem.Path.CurrentOS (encodeString)
+import Data.List (isInfixOf)
+
+mainFile :: String
+mainFile = "./.fswatch"
+
+eventPathString :: Event -> String
+eventPathString event = encodeString $ eventPath event
 
 eventType :: Event -> String
 eventType (Added    _ _) = "added"
 eventType (Modified _ _) = "modified"
 eventType (Removed  _ _) = "removed"
 
+q :: String -> String
+q str = "\"" ++ str ++ "\""
+
 handler :: Event -> IO ()
 handler event = do
-    let path  = encodeString $ eventPath event
+    let path  = eventPathString event
         eType = eventType event
-        cmd   = "sh ./.fswatch \"" ++ path  ++ "\" \"" ++ eType ++ "\""
+        cmd   = unwords [ "sh -vv"
+                        , q mainFile
+                        , q path
+                        , q eType
+                        ]
     code <- system cmd
     case code of
         ExitFailure exitCode -> putStr "Execution of helper failed. Exit code: " >> print exitCode
         _                    -> return ()
+
+predicate :: Event -> Bool
+predicate event = not $ "./.fswatch" `isInfixOf` eventPathString event
 
 main :: IO ()
 main = do
@@ -28,7 +45,7 @@ main = do
         _ <- watchTree
             mgr          -- manager
             "."          -- directory to watch
-            (const True) -- predicate
+            predicate    -- predicate
             handler      -- action
 
         forever getLine
